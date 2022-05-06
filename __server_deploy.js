58e5a5c5d45c32b8da6375d600398e1ef190a7ef
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-06 15:15:14 +0600

    sds

---
+++ __server_deploy.js
@@ -0,0 +1,18 @@
+/**
+ * --------------
+ * ONE time
+ * create heroku account
+ * verify email
+ * install  heroku cli
+ * heroku login
+ *
+ * -------------
+ * FOR EACH PROJECT
+ * -------------
+ * 1. HEROKU CREATE
+ *
+ *
+ *
+ *
+ *
+ */
